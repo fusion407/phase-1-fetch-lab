f8a56322a6297a98e87aeee70142a09e3228206f
fusion407 <fusion407@gmail.com>
2022-07-28 01:02:33 -0500

    commit tree 6bd1e2c4f24ea8b752ae73efa4cb87a655a92c95

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  const fetchPromise = fetch("https://anapioficeandfire.com/api/books");
+  return fetchPromise.then(response => {
+    return response.json();
+  }).then(books => {
+    return renderBooks(books)
+  })
 }
+  
 
 function renderBooks(books) {
   const main = document.querySelector('main');
